61d7c122a4d006a1ee16abe926294fe212479b9c
anna-d-k <nereidkino@gmail.com>
2020-04-14 23:45:05 +0300

    subtask5

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,42 @@
 package subtask5
 
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        return when (blockB) {
+            String::class -> operateStrings(blockA)
+            Int::class -> operateInts(blockA)
+            LocalDate::class -> operateDates(blockA)
+            else -> ""
+        }
+    }
+
+    private fun operateStrings(blockA: Array<Any>): String {
+        var s = ""
+        blockA.forEach {
+            if (it is String) {
+                s = s.plus(it)
+            }
+        }
+        return s
+    }
+
+    private fun operateInts(blockA: Array<Any>): Int {
+        var s = 0
+        blockA.forEach {
+            if (it is Int) {
+                s += it
+            }
+        }
+        return s
+    }
+
+    private fun operateDates(blockA: Array<Any>): LocalDate {
+        var currentDate = LocalDate.now()
+        return currentDate
     }
 }
+
