46d39643b73e660709375292581af059ff939de7
anna-d-k <nereidkino@gmail.com>
2020-04-14 21:08:40 +0300

    subtask6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,16 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val lastPair = fibonacci().asSequence().takeWhile { n >= it.first * it.second }.last()
+        return if (n == lastPair.first * lastPair.second) {
+            intArrayOf(lastPair.first, lastPair.second, 1)
+        } else {
+            intArrayOf(lastPair.second, lastPair.first.plus(lastPair.second), 0)
+        }
+    }
+
+    private fun fibonacci(): Sequence<Pair<Int, Int>> {
+        return generateSequence(Pair(0, 1), { Pair(it.second, it.first.plus(it.second)) })
     }
 }
