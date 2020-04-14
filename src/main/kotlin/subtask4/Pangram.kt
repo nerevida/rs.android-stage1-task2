6ef72f43c5b216132be590e2262b9e3e4114cb1a
anna-d-k <nereidkino@gmail.com>
2020-04-14 23:14:57 +0300

    subtask4

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,8 +2,53 @@ package subtask4
 
 class Pangram {
 
-    // TODO: Complete the following function
+    val vowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
+    val consonants = listOf(
+        'b',
+        'c',
+        'd',
+        'f',
+        'g',
+        'h',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'w',
+        'v',
+        'x',
+        'z'
+    )
+
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        return if (!inputString.isBlank()) {
+            val words = inputString.trim().split("\\s+".toRegex())
+            if (isPangram(inputString)) {
+                updateString(words, vowels)
+            } else updateString(words, consonants)
+        } else ""
+    }
+
+    private fun isPangram(inputString: String): Boolean {
+        return inputString.toLowerCase().toList().containsAll(consonants.plus(vowels))
+    }
+
+
+    private fun String.upperCase(chars: List<Char>): Any {
+        return this.map { if (it in chars) it.toUpperCase() else it }.joinToString("")
+    }
+
+    private fun updateString(words: List<String>, chars: List<Char>): String {
+        return words.asSequence()
+            .map { s -> s.prependIndent(s.count { chars.contains(it.toLowerCase()) }.toString()) }
+            .map { it.upperCase(chars) }
+            .toList()
+            .sortedBy { it.toString().get(0) }.joinToString(" ")
     }
 }
